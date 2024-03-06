ccd06ebcb4df0cae6e22d7f91ca286fb16b0e311
Aakib <aakibmansuri958@gmail.com>
2024-03-06 13:52:08 +0530

    add comments

--- src/app/formio custom components/emp table/register.ts
+++ src/app/formio custom components/emp table/register.ts
@@ -3,15 +3,36 @@ import { EmpTab } from './emp-tab.component';
 import { createCustomElement } from '@angular/elements';
 import { Components } from 'formiojs';
 
+/*
+  * Function creates and registers custom component.
+  * called from parent/app component.
+*/
 export function registerEmpTableComponent(injector: Injector) {
+  /*
+    Use the following function to create and register custom element of custom component.
+  */
   customElements.define('emp-tab', createCustomElement(EmpTab, { injector }));
+
+  /*
+    Use the following function to register custom component with the Formio.
+  */
   Components.setComponent('emptab', createCustomFormioComponent());
 }
 
+/*
+  * Dynamically creates a custom component.
+  * @returns A custom component extending in built component of formio.
+*/
 function createCustomFormioComponent() {
+  /*
+    In built component of formio.
+  */
   const BaseComponent = Components.components.input;
 
   return class CustomComponent extends BaseComponent {
+    /*
+     Define schema of custom component.
+    */
     static override schema() {
       return super.schema({
         type: 'emptab',
@@ -20,6 +41,9 @@ function createCustomFormioComponent() {
       });
     }
 
+    /*
+     This is the Form Builder information on how this component should show up within the form builder.
+    */
     static get builderInfo() {
       return {
         title: 'Emp Tab',
@@ -29,12 +53,20 @@ function createCustomFormioComponent() {
       };
     }
 
+    /*
+     * Overrides the element information.
+     * @returns custom element information.
+    */
     override elementInfo() {
       const info = super.elementInfo();
       info.type = 'emp-tab'; //must override with custom selector
       return info;
     }
 
+    /*
+     * Overrides the rendering of an element with custom information.
+     * @returns Rendered template with custom information.
+    */
     override renderElement() {
       const info = this.elementInfo();
       return this.renderTemplate('input', {
